6a1d1100378659798b22b1183a4e0a39d9f469b2
Austin Ameh <arewageek@gmail.com>
2024-06-03 04:45:55 +0100

    Successful test script for all relevant contract functions

--- contracts/Doodles.sol
+++ contracts/Doodles.sol
@@ -1,11 +1,13 @@
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import { ERC721Pausable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 
-contract Doodles is ERC721, ERC721Enumerable, Ownable {
+contract Doodles is ERC721Enumerable, Ownable {
     string public PROVENANCE;
     bool public saleIsActive = false;
     string private _baseURIextended;
@@ -17,7 +19,7 @@ contract Doodles is ERC721, ERC721Enumerable, Ownable {
 
     mapping(address => uint8) private _allowList;
 
-    constructor() ERC721("Doodles", "DOODLE") {
+    constructor() ERC721("Doodles", "DOODLE") Ownable(msg.sender) {
     }
 
     function setIsAllowListActive(bool _isAllowListActive) external onlyOwner {
@@ -47,11 +49,11 @@ contract Doodles is ERC721, ERC721Enumerable, Ownable {
         }
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+    // function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual {
+    //     super._beforeTokenTransfer(from, to, tokenId);
+    // }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
@@ -95,4 +97,5 @@ contract Doodles is ERC721, ERC721Enumerable, Ownable {
         uint balance = address(this).balance;
         payable(msg.sender).transfer(balance);
     }
+    
 }
---
+++ test/doodles.test.js
@@ -0,0 +1,155 @@
+const { expect } = require("chai");
+const { ethers, utils } = require("hardhat");
+
+describe("Doodles Smart Contract Test", function () {
+  let Doodles, addrs, addr1, addr2, addr3, owner, doodleContract;
+  beforeEach(async function () {
+    Doodles = await ethers.getContractFactory("Doodles");
+    [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
+    doodleContract = await Doodles.deploy();
+  });
+
+  describe("Contract Deployment", async function () {
+    it("Should set the right contract owner", async function () {
+      expect(await doodleContract.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Set Allowlist Active", async function () {
+    it("Should allow admin set whitelist to active", async function () {
+      const expectedState = true;
+
+      await doodleContract.connect(owner).setIsAllowListActive(expectedState);
+
+      expect(await doodleContract.isAllowListActive()).to.equals(expectedState);
+    });
+
+    it("Should revert when caller is not admin", async function () {
+      await expect(doodleContract.connect(addr3).setIsAllowListActive(true)).to
+        .be.revertedWithCustomError;
+    });
+  });
+
+  describe("Whitelist addresses", async function () {
+    const allowedMint = 3;
+    it("Should allow admin whitelist addresses", async function () {
+      await doodleContract
+        .connect(owner)
+        .setAllowList([addr1.address, addr2.address], allowedMint);
+
+      expect(await doodleContract.numAvailableToMint(addr1.address)).to.equal(
+        allowedMint
+      );
+      expect(await doodleContract.numAvailableToMint(addr2.address)).to.equal(
+        allowedMint
+      );
+    });
+
+    it("Should revert when caller is not admin", async function () {
+      await expect(
+        doodleContract
+          .connect(addr3)
+          .setAllowList([addr1.address, addr2.address], allowedMint)
+      ).to.revertedWithCustomError;
+    });
+  });
+
+  describe("Should mint a token", async function () {
+    let payment = { value: ethers.parseEther("0.123") };
+
+    it("Should revert when not in allowlist", async function () {
+      await expect(
+        doodleContract.connect(addr1).mintAllowList(1, payment)
+      ).to.be.revertedWith("Allow list is not active");
+    });
+
+    it("Should whitelist an address and mint token from that address", async function () {
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr1], 2);
+
+      await doodleContract.connect(addr1).mintAllowList(1, payment);
+
+      expect(await doodleContract.ownerOf(0)).to.equal(addr1.address);
+    });
+
+    it("Should revert when not enough fee", async function () {
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr1], 2);
+
+      await expect(
+        doodleContract
+          .connect(addr1)
+          .mintAllowList(1, { value: ethers.parseEther("0.04") })
+      ).to.be.revertedWith("Ether value sent is not correct");
+    });
+
+    it("Should revert when mint amount exceeds allowed quantity", async function () {
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr1], 2);
+
+      await expect(
+        doodleContract.connect(addr1).mintAllowList(3, payment)
+      ).to.be.revertedWith("Exceeded max available to purchase");
+    });
+  });
+
+  describe("Public mint of tokens", async function () {
+    let payment = { value: ethers.parseEther("0.123") };
+
+    // let maxSupply = await doodleContract.MAX_SUPPLY();
+    let baseUrl = "ipfs://test.io";
+
+    it("Should revert if sales is not active", async function () {
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr3], 2);
+
+      await expect(doodleContract.connect(addr2).mint(1)).to.be.revertedWith(
+        "Sale must be active to mint tokens"
+      );
+    });
+
+    it("Should revert if mint exceed max token purchase allowed", async function () {
+      await doodleContract.connect(owner).setSaleState(true);
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr3], 2);
+
+      await expect(doodleContract.connect(addr2).mint(6)).to.be.revertedWith(
+        "Exceeded max token purchase"
+      );
+    });
+
+    it("Should mint token", async function () {
+      await doodleContract.connect(owner).setSaleState(true);
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr3], 2);
+
+      await doodleContract.connect(addr3).mint(1, payment);
+
+      expect(await doodleContract.connect(addr3).ownerOf(0)).to.be.equals(
+        addr3.address
+      );
+    });
+  });
+
+  describe("Set base URI for collection", async function () {
+    const baseUri = "ipfs://tst.com";
+    const payment = { value: ethers.parseEther("0.123") };
+
+    it("Should set the base uri", async function () {
+      await doodleContract.connect(owner).setSaleState(true);
+      await doodleContract.connect(owner).setBaseURI(baseUri);
+      await doodleContract.connect(owner).setIsAllowListActive(true);
+      await doodleContract.connect(owner).setAllowList([addr2], 1);
+
+      await doodleContract.connect(addr2).mint(1, payment);
+      expect(await doodleContract.connect(addr2).tokenURI(0)).to.equal(
+        baseUri + "0"
+      );
+    });
+
+    it("Should revert if non admin sets base uri", async function () {
+      await expect(doodleContract.connect(addr1).setBaseURI(baseUri)).to.be
+        .revertedWithCustomError;
+    });
+  });
+});
